a53c9b24e99ef494028315dac0f1956132ba91bf
Jeedan <Jeedan.Cuneyt@gmail.com>
2025-06-17 21:39:06 +0300

    played around with sizing and button styling

--- frontend/src/components/ImageFetcher.jsx
+++ frontend/src/components/ImageFetcher.jsx
@@ -94,9 +94,9 @@ const ImageFetcher = () => {
 					</button>
 				</div>
 			) : currentImage ? (
-				<div className="flex flex-col items-center justify-center w-full max-w-3xl mx-auto min-h-[700px] p-4 border border-black">
+				<div className="flex flex-col items-center justify-center w-full max-w-3xl mx-auto p-4 border border-yellow-500">
 					{/* Image and Buttons Row */}
-					<div className="relative w-full max-w-3xl aspect-[4/3] flex items-center justify-center">
+					<div className="relative w-full max-w-3xl h-auto max-h-[80vh] aspect-[3/4] flex items-center justify-center border border-red-700">
 						{/* Previous Button */}
 						<div className="absolute left-2 top-1/2 -translate-y-1/2 z-20 md:static md:translate-y-0 md:top-auto md:left-auto">
 							<Button
@@ -112,11 +112,11 @@ const ImageFetcher = () => {
 							src={currentImage.imageUrl}
 							alt={currentImage.title}
 							onLoad={onLoadHandler}
-							className={`w-full h-full object-contain transition-all duration-300 ease-in-out delay-75 ${
+							className={`max-h-[70vh] w-auto max-w-full object-contain transition-all duration-300 ease-in-out delay-75 ${
 								imgLoading
 									? "opacity-0 scale-95 blur-md"
 									: "opacity-100 scale-100 blur-0"
-							}`}
+							} border border-black`}
 						/>
 
 						{/* Spinner overlay while loading */}
